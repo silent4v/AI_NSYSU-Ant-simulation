3ce646bc529005877ca0255bd3fae73aafaeaabb
daven3302 <daven3302@gmail.com>
2020-11-05 16:27:27 +0800

    add ant.h

---
+++ HW2/ant.h
@@ -0,0 +1,27 @@
+#ifndef __ANT_H__
+#define __ANT_H__
+
+class Ant
+{
+    protected:
+        bool lived;      //1 for live , 0 for dead;
+        bool job;        //0 for queen,1 for ergate,
+        int hungry_day;      //days that hungry
+    public:
+        virtual void work();
+};
+
+class Ergate:public Ant
+{
+    public:
+        void work();
+}
+
+class Queen:public Ant
+{
+    public:
+        void work();
+}
+
+
+#endif
