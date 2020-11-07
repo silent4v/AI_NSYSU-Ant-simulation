ee03ac28774012a5ba377e62c4b66238cabb7d6b
daven3302 <daven3302@gmail.com>
2020-11-07 20:11:12 +0800

    declare Point class. change position type to Point

--- HW2/ant.h
+++ HW2/ant.h
@@ -2,13 +2,24 @@
 #define __ANT_H__
 #define TIME_INTERVAL 3
 
+class Point
+{
+    public:
+        int x,y;
+        Point& operator =(Point); 
+        Point();
+        Point(int,int);
+        void set(int,int);
+        friend bool operator ==(Point,Point);
+};
+
 class Ant
 {
     protected:
         bool lived;      //1 for live , 0 for dead
         bool job;        //0 for queen,1 for ergate
         int hungry_day;      //days that hungry
-        int x , y;    //position
+        Point now;    //position
     public:
         Ant();
         virtual void work() {}
@@ -17,12 +28,12 @@ class Ant
 class Ergate:public Ant
 {
     private:    
-        bool food_detect;
-        bool sensor();      //find food,1 for food detected,0 for food-undetected
+        Point next_step;
+        Point sensor();      //find food,1 for food detected,0 for food-undetected
     public:
-        void work() {}
+        void work();
         Ergate();
-        Ergate(int,int);
+        Ergate(Point);
 };
 
 class Queen:public Ant  
@@ -31,17 +42,17 @@ class Queen:public Ant
         void work() {}
         //bool can_new();
         Queen();
-        Queen(int,int);
+        Queen(Point);
 };
 
 class Food
 {
     private:
         int days;
-        int x,y;
+        Point now;
     public:
         Food();
-        Food(int,int);
+        Food(Point);
 };
 
 class Pheromone
@@ -49,10 +60,10 @@ class Pheromone
     private:
         int days;
         int magnitude;  //min:0 , max:TIME_INTERVAL
-        int x,y;
+        Point now;
     public:
         Pheromone();
-        Pheromone(int,int);
+        Pheromone(Point);
 };
 
 #endif
