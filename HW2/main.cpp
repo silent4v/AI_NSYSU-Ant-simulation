91c7a15a78d6ed3533eb3f5c7656cd9ed351fc50
Godnaria <godnaria5182@gmail.com>
2020-11-08 21:31:43 +0800

    test

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -9,27 +9,7 @@ int main()
     file_write(tt);
     for(p = space_map.begin() ; p != space_map.end() ; p++)
         cout << p->first.x << " " << p->first.y << endl;
-    while(1)
-    {
-        
-        for(p = space_map.begin(); p != space_map.end(); p++)
-        {
-            alive = p->second->add_day();
-            if(alive)
-                p->second->work(tt);
-            else
-            {
-                
-            }
-        }
-        cout << endl;
-        map_update(space_map, cache);
-        for(p = space_map.begin() ; p != space_map.end() ; p++)
-            cout << p->first.x << " " << p->first.y << endl;
-        file_write(tt);
-        
-        cin >> temp;
-    }
+    
     delete_var(tt);
     return 0;
 }
