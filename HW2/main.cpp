3f598cf2c1def14ece31eb2d82ac7f60f3c51cad
Godnaria <godnaria5182@gmail.com>
2020-11-08 20:40:06 +0800

    w

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -4,15 +4,23 @@ using namespace std;
 int main()
 {
     Space **tt = create_space();
+    char temp;
     file_write(tt);
     for(p = space_map.begin() ; p != space_map.end() ; p++)
         cout << p->first.x << " " << p->first.y << endl;
-    cout << endl;
-    for(p = space_map.begin() ; p != space_map.end() ; p++)
-        p->second->work(tt);
-    map_update(space_map,cache);
-    for(p = space_map.begin() ; p != space_map.end() ; p++)
-        cout << p->first.x << " " << p->first.y << endl;
+    while(1)
+    {
+        
+        for(p = space_map.begin(); p != space_map.end(); p++)
+            p->second->work(tt);
+        cout << endl;
+        map_update(space_map, cache);
+        for(p = space_map.begin() ; p != space_map.end() ; p++)
+            cout << p->first.x << " " << p->first.y << endl;
+        file_write(tt);
+        
+        cin >> temp;
+    }
     delete_var(tt);
     return 0;
 }
