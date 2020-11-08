45ab57d84c19e083f2f5b844795d4581bc124731
Godnaria <godnaria5182@gmail.com>
2020-11-08 22:05:11 +0800

    fuck u main

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -3,13 +3,35 @@
 using namespace std;
 int main()
 {
-   /* Space **tt = create_space();
+    Space **tt = create_space();
     bool alive;
     char temp;
     file_write(tt);
     for(p = space_map.begin() ; p != space_map.end() ; p++)
         cout << p->first.x << " " << p->first.y << endl;
     
+    while(1)
+    {
+        for(p = space_map.begin() ; p != space_map.end() ; p++)
+        {
+            alive = p->second->add_day();
+            if(alive)
+            {
+                cout << "alive" << endl;
+                p->second->work(tt);
+                cout << "finished" << endl;
+            }
+            else
+            {
+                
+            }
+        }
+
+        map_update(space_map, cache);
+        for(p = space_map.begin() ; p != space_map.end() ; p++)
+            cout << p->first.x << " " << p->first.y << endl;
+        cin >> temp;
+    }
     delete_var(tt);
     return 0;
 }
